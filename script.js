ad810fdb34dc1f6a17ced860ae86a2a81495471f
trongmagicc <auraquang@gmail.com>
2025-11-15 12:27:32 +0700

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("demoButton").addEventListener("click", function() {
-    alert("Chúc mừng! bạn đã trúng một combo 10 cú đấm yêu thương hãy liên hệ trọng siu cấp bá đạo để nhận phần thưởng🎉");
-});
